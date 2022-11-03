a052538e8cf0c9ded932104d0f5cf1e170b9dce1
Nicholas George Klein <kleinn10@msu.edu>
2022-11-02 22:38:35 -0400

    some bs

---
+++ Synthie/xmlhelp.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "xmlhelp.h"
+
+#include <msxml2.h>
+
+/*! This function accepts a reference to a node pointer
+ *  and advances it to the next sibling node.
+ */
+inline void NextNode(CComPtr<IXMLDOMNode>& node)
+{
+    CComPtr<IXMLDOMNode> next;
+    node->get_nextSibling(&next);
+    node = next;
+}
